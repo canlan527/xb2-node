693ea8e79cabd06116c69e8f88542c435b4f0109
灿烂 <1481086138@qq.com>
2022-01-21 21:58:18 +0800

    web 服务器

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write('hello world')
+  console.log(req.headers['user-agent'])
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+  })
+  res.write(`<input />`)
+
   res.end()
 })
 
