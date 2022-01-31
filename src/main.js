35c6a3cd6bcee18412543f9758d3743d5a594bea
灿烂 <1481086138@qq.com>
2022-01-31 18:17:00 +0800

    新增演示数据

--- src/main.js
+++ src/main.js
@@ -11,3 +11,21 @@ app.get('/', (req, res) => {
   res.send('hello world');
 })
 
+// 演示数据
+const data = [
+  {
+    id: '1',
+    title: '关山月',
+    content: '明月出天山， 苍茫云海间'
+  },
+  {
+    id: '2',
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: '3',
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝'
+  },
+]
