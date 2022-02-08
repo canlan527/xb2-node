c50a70241cf290017546a3f1637b25f56e2d6861
灿烂 <1481086138@qq.com>
2022-02-08 22:25:03 +0800

    连接数据库服务

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 // 应用的入口文件
 import app from './app';
 import { APP_PORT } from './app/app.config';
-
+import { connection } from './app/database/mysql'
 /**
  * 启动服务
  */
 app.listen(APP_PORT, () => {
   console.log('🚀 服务已启动');
 });
+
+connection.connect(error => {
+  if(error) {
+    console.log('👻 连接数据服务失败', error.message);
+    return;
+  }
+
+  console.log('成功连接数据服务');
+})
