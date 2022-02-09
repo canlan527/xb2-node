6246aa0c73780bd63f3cb3b4f573746426d57f67
灿烂 <1481086138@qq.com>
2022-02-09 21:21:45 +0800

    测试创建内容接口

--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -6,4 +6,5 @@ const router = express.Router(); // 定义接口路由
 
 router.get('/posts', requestPost, postController.index); // 定义 post 接口
 
+router.post('/posts', postController.store)
 export default router; // 默认导出路由
