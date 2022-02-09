a58ae2cd02a6375b01d8001a148871f6bc425d24
灿烂 <1481086138@qq.com>
2022-02-09 17:50:12 +0800

    请求数据库的数据

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -4,11 +4,9 @@ import { getPosts } from './post.service'
 /**
  * 内容列表
  */
-export const index = (req: Request, res: Response, next: NextFunction) => {
-  // 测试使用异常处理器
-  if(req.headers.authorization !== 'SECRET') {
-    return next(new Error())
-  }
-  const posts = getPosts()
+export const index = async (req: Request, res: Response, next: NextFunction) => {
+
+  const posts = await getPosts()
+
   res.send(posts)
 }
