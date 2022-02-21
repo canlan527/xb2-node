b1dc551e7facd6ff6e004cb18b93cf063d997536
灿烂 <1481086138@qq.com>
2022-02-21 15:15:30 +0800

    身份验证模块

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'; // 引入 express
 import postRouter from '../post/post.router'; // 导入 post 路由
 import userRouter from '../user/user.router'; // 导入 user 路由
+import authRouter from '../auth/auth.router'; // 导入 user 路由
 import { defaultErrorHandler } from './app.middleware'
 /**
  * 创建 express 应用
@@ -15,7 +16,7 @@ app.use(express.json());
 /**
  * app 挂载路由
  */
-app.use(postRouter, userRouter);
+app.use(postRouter, userRouter, authRouter);
 
 /**
  * 使用通用默认异常处理器
