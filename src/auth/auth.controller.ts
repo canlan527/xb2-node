c848a13210dcc0b28c8b9c8df644481f4a97cf19
灿烂 <1481086138@qq.com>
2022-02-21 15:18:23 +0800

    定义用户登录接口

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,1 +1,13 @@
 import { Request, Response, NextFunction} from 'express';
+
+/**
+ * 用户登录接口处理器
+ */
+export const login = (req: Request, res: Response, next: NextFunction) => {
+  // 准备数据
+  const { name, password } = req.body;
+
+  // 做出响应
+  res.send({message: `欢迎回来 ${name}`});
+
+}
--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -3,4 +3,9 @@ import * as authController from './auth.controller'
 
 const router = express.Router();
 
+/**
+ * 用户登录接口
+ */
+router.post('/login', authController.login)
+
 export default router;
